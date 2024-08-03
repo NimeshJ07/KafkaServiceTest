6093d3e820d0c2aa0945093814631feb78810e5f
NimeshJ07 <nimesh.a.jethva867@gmail.com>
2024-08-03 11:59:06 +0530

    first commit

---
+++ Consumer/testConsumer/demoConsumer/demoConsumer.cs
@@ -0,0 +1,38 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ConsumerConfig
+{
+    GroupId = "test-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+};
+
+var consumer = new ConsumerBuilder<Null,string>(config).Build();
+
+consumer.Subscribe("test-topic");
+
+CancellationTokenSource cts = new CancellationTokenSource();
+
+try
+{
+    while (true)
+    {
+        var res = consumer.Consume(
+            cts.Token    
+        );
+        if(res.Message != null)
+        {
+            var demos = JsonConvert.DeserializeObject<demo>(
+                res.Message.Value
+            );
+            Console.WriteLine($"Name : {demos.name} , Age : {demos.age}");
+        }
+    }
+}
+catch (Exception)
+{
+	throw;
+}
+
+public record demo(string name, int age);
---
+++ Producer/testProducer/demoProducer/demoProducer.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+class DemoProducer
+{
+    public static async Task Main(string[] args)
+    {
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092",
+        };
+
+        using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+        try
+        {
+            string? name;
+            while ((name = Console.ReadLine()) != null)
+            {
+                var message = new Message<Null, string>
+                {
+                    Value = JsonConvert.SerializeObject(new Demo(name, 22))
+                };
+
+                var res = await producer.ProduceAsync("test-topic", message);
+
+                Console.WriteLine($"Delivered '{res.Value}' to '{res.TopicPartitionOffset}'");
+            }
+        }
+        catch (ProduceException<Null, string> ex)
+        {
+            Console.WriteLine($"Delivery failed: {ex.Error.Reason}");
+        }
+    }
+}
+
+public record Demo(string Name, int Age);
